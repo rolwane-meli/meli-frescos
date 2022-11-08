9cb489143f96f95fba95b589f947a704883ef1b5
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-08 14:45:04 -0300

    feat: criado entidade Seller

---
+++ src/main/java/com/bootcamp/melifrescos/model/Seller.java
@@ -0,0 +1,30 @@
+package com.bootcamp.melifrescos.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sellers")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+
+    @Column(length = 11, nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 45, nullable = false)
+    private String email;
+
+    @Column(length = 14, nullable = false)
+    private String cnpj;
+}
