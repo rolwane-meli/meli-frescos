b705ae5ebcf3924491c476e29ae456cc58db181d
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-08 10:38:02 -0300

    initial commit

---
+++ src/main/java/com/bootcamp/melifrescos/MeliFrescosApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.melifrescos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliFrescosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeliFrescosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/melifrescos/MeliFrescosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.melifrescos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliFrescosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
