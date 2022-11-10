6e287630a9905f10ed7f4c8d1cb0b37f8d7d56cf
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-10 09:28:38 -0300

    feat: added getById function

--- src/main/java/com/bootcamp/melifrescos/interfaces/IWarehouseService.java
+++ src/main/java/com/bootcamp/melifrescos/interfaces/IWarehouseService.java
@@ -2,6 +2,9 @@ package com.bootcamp.melifrescos.interfaces;
 
 import com.bootcamp.melifrescos.model.Warehouse;
 
+import java.util.Optional;
+
 public interface IWarehouseService {
     public Warehouse create(Warehouse warehouse);
+    public Optional<Warehouse> getById(Long id);
 }
