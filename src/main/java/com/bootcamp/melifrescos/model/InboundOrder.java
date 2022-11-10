a46f79e375f8bd3c03d954c205c87e79d3803f9f
Vitor Oliveira Silva <vitor.soliveira@mercadolivre.com>
2022-11-10 10:34:01 -0300

    chore: add anotacao @AllArgsConstructor

--- src/main/java/com/bootcamp/melifrescos/model/InboundOrder.java
+++ src/main/java/com/bootcamp/melifrescos/model/InboundOrder.java
@@ -1,6 +1,7 @@
 package com.bootcamp.melifrescos.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class InboundOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
