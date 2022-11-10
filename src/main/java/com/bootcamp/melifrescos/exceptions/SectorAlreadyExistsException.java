ac43f49ddb1e16da0cb1f17bf556b42eb216e8df
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-10 15:52:40 -0300

    feat: added Exception for sector that exists on DB

--- src/main/java/com/bootcamp/melifrescos/exceptions/SectorAlreadyExistsException.java
+++ src/main/java/com/bootcamp/melifrescos/exceptions/SectorAlreadyExistsException.java
@@ -1,2 +1,7 @@
-package com.bootcamp.melifrescos.exceptions;public class SectorAlreadyExistsException {
+package com.bootcamp.melifrescos.exceptions;
+
+public class SectorAlreadyExistsException extends RuntimeException{
+    public SectorAlreadyExistsException(String message) {
+        super(message);
+    }
 }
