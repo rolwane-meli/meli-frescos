cedf81251647f43ec946615176b6617f2de63b4f
suzy-uemura <suzy.uemura@mercadolivre.com>
2022-11-10 16:44:31 -0300

    feat: controller representante

---
+++ src/main/java/com/bootcamp/melifrescos/controller/RepresentativeController.java
@@ -0,0 +1,26 @@
+package com.bootcamp.melifrescos.controller;
+
+import com.bootcamp.melifrescos.interfaces.IRepresentativeService;
+import com.bootcamp.melifrescos.model.Representative;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/fresh-products/representative")
+public class RepresentativeController {
+
+    @Autowired
+    private IRepresentativeService service;
+
+    @PostMapping
+    public ResponseEntity<Representative> create(@RequestBody @Valid Representative representative) {
+        return new ResponseEntity<>(service.create(representative), HttpStatus.OK);
+    }
+}
