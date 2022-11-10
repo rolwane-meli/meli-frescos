029414fffbbaea6c8a110882f3ccc7ede144d7fa
ranisantos <raniel.santos@mercadolivre.com>
2022-11-10 09:47:02 -0300

    feat: corrige controller de batch

--- src/main/java/com/bootcamp/melifrescos/controller/BatchController.java
+++ src/main/java/com/bootcamp/melifrescos/controller/BatchController.java
@@ -3,6 +3,7 @@ package com.bootcamp.melifrescos.controller;
 import com.bootcamp.melifrescos.dto.BatchDTO;
 import com.bootcamp.melifrescos.interfaces.IBatchService;
 import com.bootcamp.melifrescos.model.Batch;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,10 @@ public class BatchController {
 
     @PostMapping
     public ResponseEntity<BatchDTO> create(@RequestBody BatchDTO batchDTO){
-        service.create(batchDTO);
-        return new ResponseEntity<>(batchDTO, HttpStatus.CREATED);
+        Batch result = service.create(batchDTO);
+        BatchDTO resultDto = new BatchDTO();
+        BeanUtils.copyProperties(result, resultDto);
+        resultDto.setProductId(result.getProduct().getId());
+        return new ResponseEntity<>(resultDto, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/bootcamp/melifrescos/dto/BatchDTO.java
+++ src/main/java/com/bootcamp/melifrescos/dto/BatchDTO.java
@@ -2,6 +2,7 @@ package com.bootcamp.melifrescos.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class BatchDTO {
     private Long id;
