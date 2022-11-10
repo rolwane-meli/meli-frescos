7d9ac75dcac99a82fbeab69340aaa7ce496b05aa
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-10 15:54:34 -0300

    refactor: create validations and added javadoc

--- src/main/java/com/bootcamp/melifrescos/service/SectorService.java
+++ src/main/java/com/bootcamp/melifrescos/service/SectorService.java
@@ -1,10 +1,12 @@
 package com.bootcamp.melifrescos.service;
 
+import com.bootcamp.melifrescos.dto.SectorRequestDTO;
+import com.bootcamp.melifrescos.enums.Type;
+import com.bootcamp.melifrescos.exceptions.NotFoundException;
 import com.bootcamp.melifrescos.interfaces.ISectorService;
 import com.bootcamp.melifrescos.model.Sector;
 import com.bootcamp.melifrescos.model.Warehouse;
 import com.bootcamp.melifrescos.repository.ISectorRepo;
-import com.bootcamp.melifrescos.repository.IWarehouseRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,34 +17,36 @@ import java.util.Optional;
 public class SectorService implements ISectorService {
 
     private final ISectorRepo repo;
-    private final IWarehouseRepo wareRepo;
+    private final WarehouseService wareService;
 
+    /**
+     * It creates a new sector, given a sector request DTO, and saves it to the database
+     *
+     * @param sector The object that will be created.
+     * @return A Sector object
+     */
     @Override
+    public Sector create(SectorRequestDTO sector) {
+        Long warehouseId = sector.getWarehouseId();
 
-    public Sector create(Sector sector) {
-        Long warehouseId = sector.getWarehouse().getId();
-        Long sectorId = sector.getId();
+        Warehouse warehouse = wareService.getById(warehouseId).orElse(null);
 
-        Optional<Sector> sectorOptional = getById(sectorId);
+        if (warehouse == null) throw new NotFoundException("Warehouse não existe!");
 
-        if (sectorOptional.isPresent()) return null;
-
-        if (isWarehouseExists(warehouseId)) return null;
-
-        return repo.save(sector);
+        Sector newSector = new Sector(null, sector.getName(), sector.getCapacity(), Type.fromValue(sector.getType()), warehouse, null);
+        return repo.save(newSector);
     }
 
+
+
+    /**
+     * If the sector with the given id exists, return it, otherwise return null.
+     *
+     * @param id The id of the sector you want to get.
+     * @return Optional<Sector>
+     */
     @Override
     public Optional<Sector> getById(Long id) {
        return repo.findById(id);
     }
-
-    @Override
-    public boolean isWarehouseExists(Long id) {
-        Optional<Warehouse> warehouseOptional = wareRepo.findById(id);
-
-        return warehouseOptional.isPresent();
-    }
-
-
 }
