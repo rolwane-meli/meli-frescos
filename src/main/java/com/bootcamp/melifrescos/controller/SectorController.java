1ebe9dad2b2a04bbb3ff450f1133613a8bb458e7
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-10 15:53:11 -0300

    feat: added GET route create for sector

--- src/main/java/com/bootcamp/melifrescos/controller/SectorController.java
+++ src/main/java/com/bootcamp/melifrescos/controller/SectorController.java
@@ -1,2 +1,34 @@
-package com.bootcamp.melifrescos.controller;public class SectorController {
+package com.bootcamp.melifrescos.controller;
+
+import com.bootcamp.melifrescos.dto.SectorRequestDTO;
+import com.bootcamp.melifrescos.model.Sector;
+import com.bootcamp.melifrescos.service.SectorService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/fresh-products/sector")
+@RequiredArgsConstructor
+public class SectorController {
+
+    private final SectorService service;
+
+    /**
+     * This function creates a new sector and returns it
+     *
+     * @param sector The object that will be created.
+     * @return A ResponseEntity object is being returned.
+     */
+    @PostMapping
+    public ResponseEntity<Sector> create(@RequestBody @Valid SectorRequestDTO sector) {
+        return new ResponseEntity<>(service.create(sector), HttpStatus.CREATED);
+    }
 }
