06e042b8707b13ecfd5dd27027a06142e9f3b5ed
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-10 09:29:51 -0300

    feat: implements isWarehouseExists logic

--- src/main/java/com/bootcamp/melifrescos/service/SectorService.java
+++ src/main/java/com/bootcamp/melifrescos/service/SectorService.java
@@ -2,18 +2,36 @@ package com.bootcamp.melifrescos.service;
 
 import com.bootcamp.melifrescos.interfaces.ISectorService;
 import com.bootcamp.melifrescos.model.Sector;
+import com.bootcamp.melifrescos.model.Warehouse;
 import com.bootcamp.melifrescos.repository.ISectorRepo;
+import com.bootcamp.melifrescos.repository.IWarehouseRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SectorService implements ISectorService {
 
     private final ISectorRepo repo;
+    private final IWarehouseRepo wareRepo;
 
     @Override
     public Sector create(Sector sector) {
+        Long warehouseId = sector.getWarehouse().getId();
+
+        if (isWarehouseExists(warehouseId)) return null;
+
         return repo.save(sector);
     }
+
+    @Override
+    public boolean isWarehouseExists(Long id) {
+        Optional<Warehouse> warehouseOptional = wareRepo.findById(id);
+
+        return warehouseOptional.isPresent();
+    }
+
+
 }
