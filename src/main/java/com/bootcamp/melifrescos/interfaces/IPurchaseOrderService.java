add7ae45e3a5cdb64a028b7d4fa4bb68140384f4
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-14 15:50:09 -0300

    Update IPurchaseOrderService.java
    feat: added getProductsByPurchaseOrder function to IPurchaseOrderService interface

--- src/main/java/com/bootcamp/melifrescos/interfaces/IPurchaseOrderService.java
+++ src/main/java/com/bootcamp/melifrescos/interfaces/IPurchaseOrderService.java
@@ -1,5 +1,10 @@
 package com.bootcamp.melifrescos.interfaces;
 
+import com.bootcamp.melifrescos.dto.PurchaseOrderProductDTO;
+
+import java.util.List;
+
 public interface IPurchaseOrderService {
     void updateStatusToFinished(Long id);
+    List<PurchaseOrderProductDTO> getProductsByPurchaseOrder(Long id);
 }
