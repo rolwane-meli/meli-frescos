dd6dfa22ce2f15464e97e34c70f304639715f012
Gabriel Muniz <gabriel.smuniz@mercadolivre.com>
2022-11-14 15:51:52 -0300

    Update PurchaseOrderController.java
    feat: added GET Route to show all products in purchase order

--- src/main/java/com/bootcamp/melifrescos/controller/PurchaseOrderController.java
+++ src/main/java/com/bootcamp/melifrescos/controller/PurchaseOrderController.java
@@ -1,13 +1,13 @@
 package com.bootcamp.melifrescos.controller;
 
+import com.bootcamp.melifrescos.dto.PurchaseOrderProductDTO;
 import com.bootcamp.melifrescos.interfaces.IPurchaseOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/orders")
@@ -20,5 +20,10 @@ public class PurchaseOrderController {
     public ResponseEntity<Void> updateStatusToFinished(@PathVariable Long id) {
         service.updateStatusToFinished(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<PurchaseOrderProductDTO>> getProductsByPurchaseOrder(@PathVariable Long id) {
+        return new ResponseEntity<>(service.getProductsByPurchaseOrder(id), HttpStatus.OK);
     }
 }
