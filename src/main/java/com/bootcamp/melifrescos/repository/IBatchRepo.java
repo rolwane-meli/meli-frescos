e0b098cc26d1c62b342d0f66354b806df7741398
Vitor Oliveira Silva <vitor.soliveira@mercadolivre.com>
2022-11-14 16:59:59 -0300

    feat: implementa getAllByDueDateAndCategory no  repository

--- src/main/java/com/bootcamp/melifrescos/repository/IBatchRepo.java
+++ src/main/java/com/bootcamp/melifrescos/repository/IBatchRepo.java
@@ -1,7 +1,16 @@
 package com.bootcamp.melifrescos.repository;
 
+import com.bootcamp.melifrescos.dto.BatchDTO;
+import com.bootcamp.melifrescos.enums.Type;
 import com.bootcamp.melifrescos.model.Batch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface IBatchRepo extends JpaRepository<Batch, Long> {
+    @Query("select new com.bootcamp.melifrescos.dto.BatchDTO(b.id,p.id,b.currentTemperature,b.productQuantity,b.manufacturingDate,b.manufacturingTime,b.volume,b.dueDate,b.price) from Batch b inner join Product p on b.product.id=p.id where b.dueDate between current_date() and :dueDate and p.type =:type")
+    List<BatchDTO> getAllByDueDateAndCategory(@Param("dueDate") LocalDateTime dueDate,@Param("type") Type type);
 }
