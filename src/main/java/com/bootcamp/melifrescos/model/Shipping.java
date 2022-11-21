620f48f182884a0a9ffc81e1cebfd6fca09635be
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-21 19:18:54 -0300

    feat: Shipping model

--- src/main/java/com/bootcamp/melifrescos/model/Shipping.java
+++ src/main/java/com/bootcamp/melifrescos/model/Shipping.java
@@ -1,2 +1,26 @@
-package com.bootcamp.melifrescos.model;public class Shipping {
+package com.bootcamp.melifrescos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Shipping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 2, nullable = false)
+    private String uf;
+
+    @Column(nullable = false)
+    private BigDecimal shippingPrice;
 }
