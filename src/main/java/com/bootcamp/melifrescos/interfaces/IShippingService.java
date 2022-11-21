3f1572f1bfaab623eda72be5b06701319f74a448
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-21 19:22:11 -0300

    feat: Interface ShippingService

--- src/main/java/com/bootcamp/melifrescos/interfaces/IShippingService.java
+++ src/main/java/com/bootcamp/melifrescos/interfaces/IShippingService.java
@@ -1,2 +1,7 @@
-package com.bootcamp.melifrescos.interfaces;public interface IShippingService {
+package com.bootcamp.melifrescos.interfaces;
+
+import com.bootcamp.melifrescos.dto.ShippingResponseDTO;
+
+public interface IShippingService {
+    ShippingResponseDTO getShippingPrice(String cep, Long productId, int quantity);
 }
