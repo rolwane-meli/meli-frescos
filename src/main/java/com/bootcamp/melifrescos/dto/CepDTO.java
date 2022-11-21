750f7945b32030b35c7721716f19d75c5ac0c040
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-21 19:19:17 -0300

    feat: CepDTO model

--- src/main/java/com/bootcamp/melifrescos/dto/CepDTO.java
+++ src/main/java/com/bootcamp/melifrescos/dto/CepDTO.java
@@ -1,2 +1,23 @@
-package com.bootcamp.melifrescos.dto;public class CepDTO {
+package com.bootcamp.melifrescos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CepDTO {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 }
