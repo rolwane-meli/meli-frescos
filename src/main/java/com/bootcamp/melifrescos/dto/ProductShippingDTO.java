36fd946cb90e0fa5a186b106fb5b390c6bda30b2
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-21 19:21:06 -0300

    feat: ProductShippingDTO e ShippingResponseDTO

--- src/main/java/com/bootcamp/melifrescos/dto/ProductShippingDTO.java
+++ src/main/java/com/bootcamp/melifrescos/dto/ProductShippingDTO.java
@@ -1,2 +1,21 @@
-package com.bootcamp.melifrescos.dto;public class ProductShippingDTO {
+package com.bootcamp.melifrescos.dto;
+
+import com.bootcamp.melifrescos.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductShippingDTO {
+    private Long id;
+    private String name;
+    private int quantity;
+
+    public ProductShippingDTO(Product product, int quantity) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.quantity = quantity;
+    }
 }
--- src/main/java/com/bootcamp/melifrescos/dto/ShippingResponseDTO.java
+++ src/main/java/com/bootcamp/melifrescos/dto/ShippingResponseDTO.java
@@ -1,2 +1,20 @@
-package com.bootcamp.melifrescos.dto;public class ShippingResponseDTO {
+package com.bootcamp.melifrescos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShippingResponseDTO {
+    private String cep;
+    private String UF;
+    private String locality;
+    private BigDecimal shippingPrice;
+    private ProductShippingDTO product;
 }
