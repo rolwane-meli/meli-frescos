086f91a10732546a7244eee549e1fad6adcdee82
Vitor Oliveira Silva <vitor.soliveira@mercadolivre.com>
2022-11-22 12:38:26 -0300

    ref: remove importacoes desnecessarias

--- src/main/java/com/bootcamp/melifrescos/repository/IProductRepo.java
+++ src/main/java/com/bootcamp/melifrescos/repository/IProductRepo.java
@@ -1,7 +1,6 @@
 package com.bootcamp.melifrescos.repository;
 
 import com.bootcamp.melifrescos.dto.ProductListDTO;
-import com.bootcamp.melifrescos.dto.PurchaseOrderResponse;
 import com.bootcamp.melifrescos.enums.Type;
 import com.bootcamp.melifrescos.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IProductRepo extends JpaRepository<Product, Long> {
 
