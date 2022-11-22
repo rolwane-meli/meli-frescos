99848b8138d8d236cb52aed00a7d29fca36f699f
Rolwane Borges <francisco.rolwane@mercadolivre.com>
2022-11-22 17:29:03 -0300

    ref: inboundOrderResponseDTO

--- src/main/java/com/bootcamp/melifrescos/controller/InboundOrderController.java
+++ src/main/java/com/bootcamp/melifrescos/controller/InboundOrderController.java
@@ -1,6 +1,7 @@
 package com.bootcamp.melifrescos.controller;
 
 import com.bootcamp.melifrescos.dto.InboundOrderDTO;
+import com.bootcamp.melifrescos.dto.InboundOrderResponseDTO;
 import com.bootcamp.melifrescos.interfaces.IInboundOrderService;
 import com.bootcamp.melifrescos.model.InboundOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,12 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/v1/fresh-products/inboundorder")
 public class InboundOrderController {
-
     @Autowired
     private IInboundOrderService service;
 
     @PostMapping
-    public ResponseEntity<InboundOrder> create(@RequestBody @Valid InboundOrderDTO inboundOrder) {
-        return new ResponseEntity<>(service.create(inboundOrder), HttpStatus.CREATED);
+    public ResponseEntity<InboundOrderResponseDTO> create(@RequestBody @Valid InboundOrderDTO inboundOrder) {
+        return new ResponseEntity<>(new InboundOrderResponseDTO(service.create(inboundOrder)), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
